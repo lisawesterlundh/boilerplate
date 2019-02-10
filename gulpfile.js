f452c687b7cfa65d38ed05e97fcb8a39ad2e2d85
Lisa Westerlundh <lisa.westerlundh@student.kyh.se>
2019-02-10 01:46:06 +0100

    added browser-sync

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,31 @@
 'use strict';
 var gulp = require('gulp'),
-  sass = require('gulp-sass');
+  sass = require('gulp-sass'),
+  browserSync = require('browser-sync').create();
+sass.compiler = require('node-sass');
 
 function style() {
   return gulp
-    .src('app/scss/*.scss')
+    .src('app/scss/styles.scss')
     .pipe(sass())
     .on('error', sass.logError)
-    .pipe(gulp.dest('app/css'));
+    .pipe(gulp.dest('app/dist/css'))
+    .pipe(browserSync.stream());
+}
+
+function reload() {
+  browserSync.reload();
 }
 
 function watch() {
+  browserSync.init({
+    // You can tell browserSync to use this directory and serve it as a mini-server
+    server: {
+      baseDir: 'app/dist'
+    }
+  });
   gulp.watch('app/scss/*.scss', style);
+  gulp.watch('*.html', reload);
 }
 
 exports.style = style;
